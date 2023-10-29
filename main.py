4d1fda34150209e5925d2f10dabf255dbd7cd260
Mitchell Cornall <mitch.cornall@gmail.com>
2023-10-29 19:46:03 +1100

    added the cat

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
-
-# import pygame package
 import pygame
- 
-# initializing imported module
+
 pygame.init()
- 
-# displaying a window of height
-# 500 and width 400
-pygame.display.set_mode((400, 500))
- 
-# Setting name for window
-pygame.display.set_caption('GeeksforGeeks')
- 
-# creating a bool value which checks 
-# if game is running
+
+# Screen settings
+scrn = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Displaying Cat Image')
+
+# Load the image
+imp = pygame.image.load("assets/sprites/cat.png")
+
 running = True
- 
-# Game loop
-# keep game running till running is true
 while running:
-   
-    # Check for event if user has pushed 
-    # any event in queue
     for event in pygame.event.get():
-       
-        # if event is of type quit then set
-        # running bool to false
         if event.type == pygame.QUIT:
             running = False
+
+    # Blit the image on every loop iteration
+    scrn.blit(imp, (0, 0))
+
+    # Update the display
+    pygame.display.flip()
+
+pygame.quit()
