6db99964773a9175212694e56aff5d70e9d0da56
Mitchell Cornall <mitch.cornall@gmail.com>
2023-10-31 16:17:33 +1100

    added level decode algorithm

---
+++ openlevel.py
@@ -0,0 +1,21 @@
+def openlevelfile(level):
+    f = open(level,"r")
+
+    x=0                       #FUNCTION TO TAKE FILENAME "LEVEL.DAT" AND RETURN LIST WITH ELEMENTS FOR EACH ROW.
+    columns = []              #ROW ELEMENTS ARE LISTS WITH ELEMENTS OF CHARACTERS IN ROW
+
+    while True:
+        line = f.readline()
+        row = []
+        i=1
+        for element in line:
+            if(element != " "):
+                row.append(element)
+            i+=1 
+        print(row)
+        columns.append(row)
+        if(row):
+            print("e")
+        else:
+            break
+    return columns
